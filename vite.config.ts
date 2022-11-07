c864a86454e3f4504a5cb5912fb6eaa636021b89
JP <cala.js@icloud.com>
2022-11-07 13:41:53 -0500

    Added path aliases

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: path.resolve(__dirname, 'src'),
+            },
+            {
+                find: '@interfaces',
+                replacement: path.resolve(__dirname, 'src/entities/interfaces'),
+            },
+            {
+                find: '@classes',
+                replacement: path.resolve(__dirname, 'src/entities/classes'),
+            },
+            {
+                find: '@types',
+                replacement: path.resolve(__dirname, 'src/entities/types'),
+            },
+            {
+                find: '@utils',
+                replacement: path.resolve(__dirname, 'src/utils'),
+            },
+            {
+                find: '@config',
+                replacement: path.resolve(__dirname, 'src/config'),
+            },
+            {
+                find: '@views',
+                replacement: path.resolve(__dirname, 'src/views'),
+            },
+        ],
+    },
+})
